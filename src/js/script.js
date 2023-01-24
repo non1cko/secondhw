15f35eed79ab96e45770341eaf555e3569932174
Nonicko <yehkfy7@mail.ru>
2023-01-24 19:15:46 +0600

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-    $('.carousel_item').slick();
+    $('.carousel__item').slick({
+        speed: 500,
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev"><img id="arrows_car" src="../icons/l-arr.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img id="arrows_car" src="../icons/r-arr.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
 });
+
